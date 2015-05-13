d47f0921b883a59cbd57c0e1082573b75c03894f
JJ Chai <chaij@umail.iu.edu>
2015-05-13 15:32:16 -0400

    Start testing a new way to split big fasta into chunks according to the groups

---
+++ database_src/ReadFile.h
@@ -0,0 +1,28 @@
+#ifndef READFILE_H
+#define READFILE_H
+
+/********************************************************************
+ * ReadFile.h
+ * Created by JJ Chai  on 05/13/2015.
+ * Copyright (c) 2015 JJ Chai (ORNL). Allrights reserved.
+********************************************************************/
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <map>
+#include <unordered_map>
+#include <sys/stat.h>
+using namespace std;
+
+namespace PfClust
+{
+}
+
+#endif //READFILE.HPP
