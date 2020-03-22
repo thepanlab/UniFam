3cbd376a5a9722921db2c11dc7f4687fe622d201
JJ Crosskey <crosskey.jj@gmail.com>
2020-03-22 15:33:08 -0400

    Reformat UniFam.py, use f string

--- src/unifam/annotate/UniFam.py
+++ src/unifam/annotate/UniFam.py
@@ -1,12 +1,5 @@
 '''
-UniFam.py
-
-pipeline
-
-Created by JJ Chai on 02/24/2014
-Last modified Mon Apr 20 16:58:56 EDT 2015
-Copyright (c) 2014 JJ Chai (ORNL). All rights reserved.
-
+Entrance point for running unifam pipeline.
 '''
 # Import Python modules
 import argparse
@@ -14,16 +7,17 @@ import configparser
 from datetime import datetime
 import sys
 
-import UniFam_lib  # in this directory
-import UniFam_lib_batch
+from unifam.annotate.UniFam_lib import UniFam as unifam
+from unifam.annotate.UniFam_lib_batch import UniFam as batch_unifam
 
 
 # Import local modules
 # Version
 version_str = "1.1.0"
 ''' 0.0.1.  first version of pipeline, including prodigal, hmmsearch, and annotation
-            all configuration options are in a file. User can(and must) change the options in the config file to customize.
-    0.0.2.  Added more information for pathologic module of the analysis, and rRNA, tRNA            
+            all configuration options are in a file. User can(and must) change the options
+            in the config file to customize.
+    0.0.2.  Added more information for pathologic module of the analysis, and rRNA, tRNA
             analysis.
     1.0.0   First Stable version for release, UniFam 1.0.0
     1.1.0   Added README file to describe output files; zip pathway inference output results
@@ -31,35 +25,27 @@ version_str = "1.1.0"
 '''
 
 parser = argparse.ArgumentParser(description="Annotation of contigs/proteins using UniFam",
-                                 prog='UniFam',  # program name
-                                 prefix_chars='-',  # prefix for options
-                                 fromfile_prefix_chars='@',  # if options are read from file, '@args.txt'
-                                 conflict_handler='resolve',  # for handling conflict options
-                                 add_help=True,  # include help in the options
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter  # print default values for options in help message
+                                 prog='UniFam',
+                                 prefix_chars='-',
+                                 fromfile_prefix_chars='@',
+                                 conflict_handler='resolve',
+                                 add_help=True,
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter
                                  )
 # version control
 parser.add_argument("--version", action="version", version='%(prog)s {}'.format(version_str))
 
 # --verbose mode, default: false (quiet mode)
 parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode, more output")
-#parser.add_argument("-n", "--dryrun", action="store_true",help="dryrun, only print commands, do not execute")
-
+parser.add_argument("-n", "--dryrun", action="store_true", help="dryrun, only print commands, do not execute")
 parser.add_argument("-b", "--batch", action="store_true", help="batch mode to construct pathway", dest="batch")
 
-# input files and directories
 # configuration file, required
 parser.add_argument("-c", help="configuration file", dest='configFile', required=True)
 # input file, required
 parser.add_argument("-i", help="input fasta file (contig or protein fasta/faa file)", dest='inputFile', required=True)
 
 
-# output file, now this is removed, determined from the prefix argument instead
-## parser.add_argument("-o",help="output annotation file",dest='outputFile',required=True)
-
-# =================================================================
-# main function
-# =================================================================
 def main(argv=None):
 
     if argv is None:
@@ -67,17 +53,15 @@ def main(argv=None):
 
     # print some information
     if args.verbose:
-        sys.stdout.write('running verbosely\n')
-        sys.stdout.write('configuration file is: {0}\n'.format(args.configFile))
-        sys.stdout.write('input fasta file is: {0}\n'.format(args.inputFile))
-        #sys.stdout.write('output file is: {}\n'.format(args.outputFile))
-    else:
-        sys.stdout.write('\n')
+        sys.stdout.write('Running verbosely\n'
+                         f'configuration file = {args.configFile}\n'
+                         f'input fasta file = {args.inputFile}\n'
+                         f'batch mode = {args.batch}')
 
     # display work start, and time record
     start_time = datetime.now()
     sys.stderr.write("\n===============================================================================\n")
-    sys.stderr.write("Welcome to UniFam v{0}: \n".format(version_str))
+    sys.stderr.write(f"Welcome to UniFam v{version_str}: \n")
 
     # read configuration file
     config = configparser.ConfigParser()
@@ -85,22 +69,17 @@ def main(argv=None):
 
     # Annotating with UniFam
     if args.batch:
-        UniFam_lib_batch.UniFam(args.inputFile, config, args.verbose)
+        batch_unifam(args.inputFile, config, args.verbose)
     else:
-        UniFam_lib.UniFam(args.inputFile, config, args.verbose)
+        unifam(args.inputFile, config, args.verbose)
 
     # write the configuration file to standard output for checking
     # config.write(sys.stdout)
 
-    # display work end, and time record, elapsed time
     finish_time = datetime.now()
     duration = finish_time - start_time
     sys.stderr.write("\nTotal Elapsed Time = [%s] \n" % duration)
     sys.stderr.write("===============================================================================\n")
-
-# ==============================================================
-# call from command line (instead of interactively)
-# ==============================================================
 
 
 if __name__ == '__main__':
