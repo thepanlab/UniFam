cce6ee00159c44788613b90fcee2345e0da7d3da
JJ Crosskey <crosskey.jj@gmail.com>
2020-05-25 16:43:07 -0400

    Finish writing all input files for pathologic

--- src/unifam/annotate/pathologic_input.py
+++ src/unifam/annotate/pathologic_input.py
@@ -4,6 +4,7 @@ Prepare input files for Pathologic input
 from unifam.base.util import SysUtil
 import logging
 import pandas as pd
+import os
 
 
 class PathoLogicInput(object):
@@ -74,16 +75,105 @@ class PathoLogicInput(object):
         return patho_dict
 
     @classmethod
-    def get_prot_patho_dict_list(cls, domtab_df, contig_to_info, cluster_annot_dict):
-        """
-        From hmmsearch result `domtab_df` (protein --> hmm),
-        prodigal output gbk result `contig_to_info` (protein --> start, end bases)
-        `cluster_annot_dict`, generate protein annotations in pathologic dict format
-        """
-        assert isinstance(domtab_df, pd.DataFrame), type(domtab_df)
+    def write_genetic_element(cls, genetic_element_file,
+                              contig_to_info, domtab_df, cluster_annot_dict,
+                              rrna_df, trna_df):
+        '''
+        Write genetic-elements.dat file with the presence of prodigal's output file,
+        which provides more information about the contigs/sequences in the genome
+
+        Input:
+        1.  genetic_elem - file object to write the content
+        2.  contig_to_info
+
+        Output:
+        1.  write content to the genetic-elements.dat
+        '''
+        patho_dir = os.path.dirname(genetic_element_file)
+        SysUtil.mkdir_p(patho_dir)
+        # counter for each type of sequence
+        plasmid_ct = 0
+        chrsm_ct = 0
+        contig_ct = 0
+        contig_to_annot_file = dict()
+
+        with open(genetic_element_file, 'w') as genetic_elem:
+            for i, contig_info in contig_to_info.items():
+                seq_type = contig_to_info[i]['seqType']
+                if seq_type == "CHRSM":
+                    chrsm_ct += 1
+                    element_id = seq_type + "-" + str(chrsm_ct)
+                elif seq_type == "PLASMID":
+                    plasmid_ct += 1
+                    element_id = seq_type + "-" + str(plasmid_ct)
+                elif seq_type == 'CONTIG':
+                    contig_ct += 1
+                    element_id = seq_type + "-" + str(contig_ct)
+                else:
+                    raise ValueError(f'seq_type {seq_type} must be CHRSM, PLASMID, or CONTIG')
+
+                seq_id = contig_to_info[i]['seqID']
+                genetic_elem.write(f'ID\t{element_id}\n')
+                genetic_elem.write(f'NAME\t{seq_id}\n')
+                genetic_elem.write(f'TYPE\t:{seq_type}\n')
+                genetic_elem.write(f'CIRCULAR?\t{contig_info["circular"]}\n')
+
+                annot_file = f'{element_id}.pf'
+                contig_to_annot_file[i] = annot_file
+                annot_file_path = os.path.join(patho_dir, annot_file)
+
+                cls._write_rna_annot_file(annot_file_path, rrna_df, seq_id, 'rrna', 'w')
+                cls._write_rna_annot_file(annot_file_path, trna_df, seq_id, 'trna', 'w')
+                cls._write_protein_annot_file(annot_file_path, i, contig_info,
+                                              domtab_df, cluster_annot_dict, 'a')
+
+                genetic_elem.write(f'ANNOT-FILE\t{annot_file}\n')
+                genetic_elem.write(f'SEQ-FILE\t \n')
+                genetic_elem.write("//\n")
+                print(f'{seq_id} writtein to {annot_file}')
+        logging.info(f'genetic_element info is written to {genetic_element_file}')
 
     @classmethod
-    def rrna_df_to_patho_dict(cls, rrna_series, seq_name_to_id):
+    def _write_protein_annot_file(cls, annot_file, contig_num, contig_info,
+                                  domtab_df, cluster_annot_dict, file_mode='a'):
+        """
+        Write protein annotations for a contig, with the following informatin:
+        * `contig_num` contig_info is an item pair from contig_to_info parsed from gbk file
+        * `domtab_df` provides the mapping from protein sequence name to best matching hmm name
+        * `cluster_annot_dict` provides the mapping from hmm name to the corresponding cluster's annotation
+        """
+        assert isinstance(domtab_df, pd.DataFrame), type(domtab_df)
+        seq_id = contig_info['seqID']
+        with open(annot_file, file_mode) as annot_pf:
+            for protein_num, cds in contig_info['proteins'].items():
+                protein_seq_name = f'{seq_id}_{protein_num}'
+                start_pos, end_pos = cds
+                protein_subdf = domtab_df[domtab_df['seq_name'] == protein_seq_name]
+                if protein_subdf.empty:
+                    continue
+                assert len(protein_subdf) == 1, (protein_seq_name, protein_subdf)
+                protein_hmm = protein_subdf['hmm_name'].values[0]
+                protein_patho_dict = cls.prot_cluster_annot_dict_to_patho_dict(
+                    cluster_annot_dict[protein_hmm])
+                protein_patho_dict['STARTPOS'] = str(start_pos)
+                protein_patho_dict['ENDPOS'] = str(end_pos)
+                protein_patho_dict['NAME'] = protein_seq_name
+                protein_patho_dict['ID'] = f'{contig_num}_protein_{protein_num}'
+                annot_pf.write(cls.pf_str_from_patho_dict(protein_patho_dict))
+
+    @classmethod
+    def _write_rna_annot_file(cls, annot_file, rna_df, seq_name, rna_type, file_mode='a'):
+        assert rna_type in ('trna', 'rrna'), rna_type
+        seq_sub_df = rna_df[rna_df['seq_name'] == seq_name]
+        if not seq_sub_df.empty:
+            with open(annot_file, file_mode) as annot_pf:
+                for _, r in seq_sub_df.iterrows():
+                    patho_dict = PathoLogicInput.rrna_df_to_patho_dict(r) if rna_type == 'rrna' \
+                            else PathoLogicInput.trna_df_to_patho_dict(r)
+                    annot_pf.write(PathoLogicInput.pf_str_from_patho_dict(patho_dict))
+
+    @classmethod
+    def rrna_df_to_patho_dict(cls, rrna_series):
         """
         Convert RNAmmer output parse result df to pathologic key,val pair dict.
         This dict will then translate to a record in the annotation .pf file
@@ -93,10 +183,10 @@ class PathoLogicInput(object):
 
         """
         assert isinstance(rrna_series, pd.Series), type(rrna_series)
-        assert isinstance(seq_name_to_id, dict), type(seq_name_to_id)
         patho_dict = dict()
         seq_name = rrna_series['seq_name']
-        patho_dict['NAME'] = seq_name_to_id[seq_name]
+        rrna_idx = rrna_series['rRNA_idx']
+        patho_dict['NAME'] = f'rRNA_{rrna_idx}'
         patho_dict['ID'] = f'{seq_name}_rRNA_{rrna_series["attribute"]}'
         strand_pm = rrna_series['pm']
         start_base = rrna_series['start'] if strand_pm == '+' else rrna_series['end']
@@ -107,7 +197,7 @@ class PathoLogicInput(object):
         return patho_dict
 
     @classmethod
-    def trna_df_to_patho_dict(cls, trna_series, seq_name_to_id):
+    def trna_df_to_patho_dict(cls, trna_series):
         """
         Convert tRNAscan output parse result df to pathologic key,val pair dict.
         This dict will then translate to a record in the annotation .pf file
@@ -119,10 +209,9 @@ class PathoLogicInput(object):
 3     NODE_1_length_526561_cov_50.907785          4      523248    523332       Ser        TGA             0           0
         """
         assert isinstance(trna_series, pd.Series), type(trna_series)
-        assert isinstance(seq_name_to_id, dict), type(seq_name_to_id)
         patho_dict = dict()
         seq_name = trna_series['seq_name'].strip()
-        patho_dict['NAME'] = f"{seq_name_to_id[seq_name]}_{trna_series['tRNA_idx']}"
+        patho_dict['NAME'] = f"tRNA_{trna_series['tRNA_idx']}"
         patho_dict['ID'] = f"{seq_name}_tRNA_idx_{trna_series['tRNA_idx']}"
         patho_dict['STARTBASE'] = str(trna_series['tRNA_begin'])
         patho_dict['ENDBASE'] = str(trna_series['tRNA_end'])
@@ -147,12 +236,12 @@ class PathoLogicInput(object):
                     pf_str_list.append(f'{key}\t{val_element}')
             else:
                 raise TypeError(f'{type(val)} is not str or list')
-        pf_str_list.append('//')
+        pf_str_list.append('//\n')
         return '\n'.join(pf_str_list)
 
     @classmethod
     def write_organism_params(cls, file_path, org_param_dict):
-        SysUtil.mkdir_p(file_path)
+        SysUtil.mkdir_p(os.path.dirname(file_path))
         sep = '\t'
         with open(file_path, 'w') as org_param_f:
             for key, val in org_param_dict.items():
